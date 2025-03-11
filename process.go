15a15eead0ce11a4b857d307c6e8a410478fc075
Mohammad <m.abdollahzadeh.a@gmail.com>
2025-03-11 15:06:18 +0330

    Feature: Save the initialSegment result to a file

--- process.go
+++ process.go
@@ -1,23 +1,21 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
-	"github.com/nats-io/nats.go"
 	"log"
+	"os"
 )
 
 func process() {
-	// Connect to the NATS server
 	nc, err := nats.Connect(nats.DefaultURL)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer nc.Close()
 
-	// Define the subject to subscribe to
 	subject := "mp4FilePaths"
 
-	// Create a channel to receive NATS messages
 	msgChan := make(chan *nats.Msg, 1024) // Buffered channel to avoid blocking
 
 	// Subscribe to the NATS subject using ChanSubscribe
@@ -26,22 +24,18 @@ func process() {
 		log.Fatal(err)
 	}
 	defer func() {
-		// Drain the subscription before unsubscribing
 		if err := sub.Drain(); err != nil {
 			log.Printf("Error draining subscription: %v\n", err)
 		}
-		// Unsubscribe after draining
 		sub.Unsubscribe()
 	}()
 
-	// Keep the program running to receive messages
 	fmt.Println("Subscribed to", subject)
 
 	for msg := range msgChan {
 		filePath := string(msg.Data)
 		fmt.Printf("Received file path: %s\n", filePath)
 
-		// Process the file
 		boxes, err := ExtractInitializationSegment(filePath)
 		if err != nil {
 			fmt.Println("Error:", err)
@@ -50,6 +44,54 @@ func process() {
 
 		for _, box := range boxes {
 			fmt.Printf("MP4Box Type: %s, Size: %d\n", box.Type, box.Size)
+
+		}
+
+		err = writeResultIntoFile("output.mp4", boxes)
+		if err != nil {
+			return
 		}
 	}
+}
+
+func writeResultIntoFile(fileName string, boxes []*MP4Box) error {
+	file, err := os.Create(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+
+		}
+	}(file)
+
+	for _, box := range boxes {
+		err := writeBox(file, box)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return nil
+}
+
+func writeBox(file *os.File, box *MP4Box) error {
+	// Write the Size field (4 bytes)
+	if err := binary.Write(file, binary.BigEndian, box.Size); err != nil {
+		return err
+	}
+
+	typeBytes := []byte(box.Type)
+	if len(typeBytes) != 4 {
+		panic("Type field must be exactly 4 bytes")
+	}
+	if _, err := file.Write(typeBytes); err != nil {
+		return err
+	}
+
+	if _, err := file.Write(box.Data); err != nil {
+		return err
+	}
+
+	return nil
 }
